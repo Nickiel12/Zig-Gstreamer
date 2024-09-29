7b077ed71343fe8d7b9e620639f07f6200680cd8
Nickiel12 <nickiel@nickiel.net>
2024-09-29 05:26:47 +0000

    moved to gstreamer elements

--- src/main.zig
+++ src/main.zig
@@ -5,16 +5,42 @@ const gst = @cImport({
 const glib = @cImport({
     @cInclude("glib.h");
 });
+const gobj = @cImport({
+    @cInclude("glib-object.h");
+});
 
 pub fn main() void {
+    // This allows me to utilize the same command line args and gstreamer
     gst.gst_init(@ptrCast(&std.os.argv.len), @ptrCast(&std.os.argv.ptr));
 
-    const pipeline: *gst.GstElement = gst.gst_parse_launch(
-        "playbin uri=https://www.freedesktop.org/software/gstreamer-sdk/data/media/sintel_trailer-480p.webm",
-        null,
-    );
+    const source: ?*gst.GstElement = gst.gst_element_factory_make("videotestsrc", "source");
+    const sink: ?*gst.GstElement = gst.gst_element_factory_make("autovideosink", "sink");
+
+    const pipeline: ?*gst.GstElement = gst.gst_pipeline_new("test-pipeline");
+
+    if (source == null or sink == null or pipeline == null) {
+        std.debug.panic("Not all elements could be created!", .{});
+    }
+
+    // const bin: ?*gst.GstBin = gobj.G_TYPE_CHECK_INSTANCE_CAST(pipeline, gst.GST_TYPE_BIN, gst.GstBin);
+    const bin: *gst.GstBin = @ptrCast(pipeline);
+    // gst.g_object_get(gst.G_OBJECT(pipeline), "bin", &bin, null);
+
+    gst.gst_bin_add_many(bin, source, sink, null);
+    if (gst.gst_element_link(source, sink) != true) {
+        gst.gst_object_unref(pipeline);
+        std.debug.panic("Elements could not be linked", .{});
+    }
+
+    gst.g_object_set(source, "pattern", 0, null);
+
+    const ret = gst.gst_element_set_state(pipeline, gst.GST_STATE_PLAYING);
+    if (ret == gst.GST_STATE_CHANGE_FAILURE) {
+        gst.gst_object_unref(pipeline);
+        std.debug.panic("Could not start pipeline", .{});
+    }
+
     const bus: *gst.GstBus = gst.gst_element_get_bus(pipeline);
-    _ = gst.gst_element_set_state(pipeline, gst.GST_STATE_PLAYING);
     const msg: *gst.GstMessage = gst.gst_bus_timed_pop_filtered(
         bus,
         gst.GST_CLOCK_TIME_NONE,
@@ -22,7 +48,20 @@ pub fn main() void {
     );
 
     if (gst.GST_MESSAGE_TYPE(msg) == gst.GST_MESSAGE_ERROR) {
-        std.debug.print("an error ocurred bla bla bla\n", .{});
+        var err: ?*gst.GError = null;
+        var debug_info: ?*gst.gchar = null;
+
+        switch (gst.GST_MESSAGE_TYPE(msg)) {
+            gst.GST_MESSAGE_ERROR => {
+                gst.gst_messsage_parse_error(msg, &err, &debug_info);
+                std.debug.print("Error received from element {s}: {s}", .{ gst.GST_OBJECT_NAME(msg.src), err.message });
+                std.debug.print("Debugging information: {s}", .{debug_info orelse "none"});
+                gst.g_clear_error(&err);
+                gst.g_free(debug_info);
+            },
+            else => {},
+        }
+        gst.gst_message_unref(msg);
     }
 
     gst.gst_object_unref(bus);
